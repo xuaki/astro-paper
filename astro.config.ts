db917522dd7bce8f5788c4f3265a8f6a232d4d52
xuaki <92970596+xuaki@users.noreply.github.com>
2024-10-07 15:36:31 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,6 @@ import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
 import sitemap from "@astrojs/sitemap";
 import { SITE } from "./src/config";
-import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
